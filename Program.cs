5d76d267f2eb8d902f1b62d2cd4e35fd8ed27b84
Colin Thomas <cthomas0687@gmail.com>
2020-11-29 23:40:56 -0500

    Conversion logic should be good to go. Stick it in another file

--- Program.cs
+++ Program.cs
@@ -9,64 +9,18 @@ namespace get_audio
 {
     class Program
     {
-        // "https://www.yt-download.org/download/E-C22VCWB-U/mp3/320/1606705182/63a9a1d2fdf778f9ed5ca2cd556abfec5c96ac634fef2ff893e44003dbf5f94f/0"
-
         static void Main(string[] args)
         {
             Console.WriteLine("Calling API...");
 
-            GetAudio("https://www.youtube.com/watch?v=3nINDTxu5eY");
+            string link = ConvertToLink("https://www.youtube.com/watch?v=3nINDTxu5eY").Result;
+            DownloadFromLink(link);
 
             Console.Write("Enter 'q' to finish");
             while (Console.ReadLine() != "q") ;
 
         }
 
-        static async Task GetAudio(string url)
-        {
-            HttpClient client = new HttpClient();
-
-            Console.WriteLine("Posting");
-
-            var key = GetKey(url);
-
-            HttpResponseMessage response = await client.GetAsync("https://www.yt-download.org/api/button/mp3/" + key);
-
-            var message = await response.Content.ReadAsStringAsync();
-
-            var linkParser = new Regex(@"\b(?:https?://www.yt-download.org/download/" + @key + @"/mp3/320/" + @")\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
-            string link = linkParser.Match(message).Value;
-
-            Console.WriteLine("Link used = " + link);
-
-
-            // Get file after link is generated with key
-
-            WebClient wc = new WebClient();
-            
-            
-            await wc.DownloadFileTaskAsync(new Uri(link), "C:\\Users\\Owner\\Downloads\\song.mp3");
-
-            Console.WriteLine("Download Complete");
-
-            
-          
-        }
-
-        static string GetKey(string url)
-        {
-            string pattern = "https://www.youtube.com/watch?v=";
-
-            int pos = url.IndexOf(pattern);
-            string key = url.Substring(pos + pattern.Length);
-            //int index = keyhalf.IndexOf('&');
-            //string key = keyhalf.Substring(0, index);
-
-            Console.WriteLine(key);
-
-            return key;
-
-        }
+        
     }
 }
---
+++ YTHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace get_audio
+{
+    public class YTHelper
+    {
+        public static async Task<String> ConvertToLink(string url)
+        {
+            var key = GetKey(url);
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://www.yt-download.org/api/button/mp3/" + key);
+            var message = await response.Content.ReadAsStringAsync();
+
+            return GetLink(message, url);
+
+        }
+
+        public static async Task DownloadFromLink(string downloadLink)
+        {
+            WebClient wc = new WebClient();
+            string name = await GetName(downloadLink);
+            await wc.DownloadFileTaskAsync(new Uri(downloadLink), "C:\\Users\\Owner\\Downloads\\" + name);
+
+            Console.WriteLine("\nDownload Complete");
+        }
+
+        private static string GetLink(string message, string url)
+        {
+            var key = GetKey(url);
+
+            var linkExpression = new Regex(@"\b(?:https?://www.yt-download.org/download/" + @key + @"/mp3/320/" + @")\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return linkExpression.Match(message).Value;
+        }
+
+        private static string GetKey(string url)
+        {
+            string pattern = "https://www.youtube.com/watch?v=";
+
+            int pos = url.IndexOf(pattern);
+            string key = url.Substring(pos + pattern.Length);
+
+            // Currently, this will not work on videos that are part of a playlist. Probably due to the parsing on the API end
+            //int index = keyhalf.IndexOf('&');
+            //string key = keyhalf.Substring(0, index);
+
+            return key;
+        }
+
+        private static async Task<string> GetName(string downloadLink)
+        {
+            WebResponse nameResponse = await WebRequest.Create(new Uri(downloadLink)).GetResponseAsync();
+            string name = nameResponse.Headers["Content-Disposition"].Substring(nameResponse.Headers["Content-Disposition"].IndexOf("filename") + 9).Replace("\"", "");
+
+            return name;
+        }
+    }
+}
